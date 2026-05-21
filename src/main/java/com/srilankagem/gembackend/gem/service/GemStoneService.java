39cf95d715ed75a525c902b0673e2fe3be53e7d9
vishwageethanjana <geeth.me67@gmail.com>
2026-05-21 08:28:44 +0530

    Gem backend java 20.05.2026

--- src/main/java/com/srilankagem/gembackend/gem/service/GemStoneService.java
+++ src/main/java/com/srilankagem/gembackend/gem/service/GemStoneService.java
@@ -1,5 +1,7 @@
 package com.srilankagem.gembackend.gem.service;
 
+import com.srilankagem.gembackend.common.exception.ResourceNotFoundException;
+import com.srilankagem.gembackend.gem.dto.GemStoneRequest;
 import com.srilankagem.gembackend.gem.dto.GemStoneResponse;
 import com.srilankagem.gembackend.gem.models.GemStone;
 import com.srilankagem.gembackend.gem.repository.GemStoneRepo;
@@ -18,6 +20,31 @@ public class GemStoneService {
 
 //      return gemStoneRepo.findByActiveTrue(pageable).map(item ->toResponse(item));
         return gemStoneRepo.findByActiveTrue(pageable).map(this::toResponse);
+    }
+
+    public GemStoneResponse createGemStone(GemStoneRequest request) {
+
+        GemStone gemStone = GemStone.builder()
+                .gemCode(request.getGemCode())
+                .type(request.getType())
+                .color(request.getColor())
+                .caratWeight(request.getCaratWeight())
+                .origin(request.getOrigin())
+                .treatment(request.getTreatment())
+                .pricePerCarat(request.getPricePerCarat())
+                .stockQuantity(request.getStockQuantity())
+                .certified(request.isCertified())
+                .description(request.getDescription())
+                .active(true)
+                .build();
+
+        return toResponse(gemStoneRepo.save(gemStone));
+    }
+
+    public GemStoneResponse getGemShoneById(Long id) throws ResourceNotFoundException {
+        return toResponse(gemStoneRepo.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Gem" , id.toString())
+        ));
     }
 
     private GemStoneResponse toResponse(GemStone gemStone) {
