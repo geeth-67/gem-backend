37f6b033b3d07f80e6c8a58905d9293d5aade355
vishwageethanjana <geeth.me67@gmail.com>
2026-05-25 13:37:16 +0530

    Gem backend java 25.05.2026

--- src/main/java/com/srilankagem/gembackend/gem/service/CertificateService.java
+++ src/main/java/com/srilankagem/gembackend/gem/service/CertificateService.java
@@ -3,6 +3,7 @@ package com.srilankagem.gembackend.gem.service;
 import com.srilankagem.gembackend.common.exception.DuplicateResourceException;
 import com.srilankagem.gembackend.gem.dto.CertificateRequest;
 import com.srilankagem.gembackend.gem.repository.CertificateRepo;
+import jakarta.validation.constraints.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,11 @@ public class CertificateService {
     public CertificateRepo createCertificate(CertificateRequest request) {
 
         if (certificateRepo.existsByCertificateNumber(request.getCertificateNumber())) {
-            throw new DuplicateResourceException("Certificate with this number already exists");
+            throw new DuplicateResourceException("Certificate with" + request.getCertificateNumber() + "this number already exists");
         }
+        if (certificateRepo.existsByGemStoneId(request.getGemId())) {
+            throw new DuplicateResourceException("Certificate with" + request.getGemId() + "already exists");
+        }
+        return null;
     }
 }
