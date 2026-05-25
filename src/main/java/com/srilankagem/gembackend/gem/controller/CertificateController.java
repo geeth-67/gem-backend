991aaf3bb18e0bac3f0b2f876da4b9d1d5a79ffb
vishwageethanjana <geeth.me67@gmail.com>
2026-05-25 15:16:38 +0530

    Gem backend java 25.05.2026

--- src/main/java/com/srilankagem/gembackend/gem/controller/CertificateController.java
+++ src/main/java/com/srilankagem/gembackend/gem/controller/CertificateController.java
@@ -6,9 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/certificate")
@@ -21,9 +19,18 @@ public class CertificateController {
         this.certificateService = certificateService;
     }
 
+    @PostMapping
     public ResponseEntity<CertificateResponse> createCertificate(@Valid @RequestBody CertificateRequest request) {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(certificateService.createCertificate(request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CertificateResponse> getCertificateById(@PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                certificateService.getCertificateById(id)
+        );
     }
 }
