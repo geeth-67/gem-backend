23ad1ea1239b037574d9fb1cd4297f1c15fd4c22
vishwageethanjana <geeth.me67@gmail.com>
2026-06-01 10:46:29 +0530

    Gem backend java 01.06.2026

---
+++ src/main/java/com/srilankagem/gembackend/trade/service/TradeService.java
@@ -0,0 +1,9 @@
+package com.srilankagem.gembackend.trade.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TradeService {
+}
