b08043adbb0c8d0e4bdb648c10641288ec3be3d7
vishwageethanjana <geeth.me67@gmail.com>
2026-06-01 11:43:03 +0530

    GemBackend java - M TO M relation 01.06.2026

---
+++ src/main/java/com/srilankagem/gembackend/gem/controller/TagController.java
@@ -0,0 +1,60 @@
+package com.srilankagem.gembackend.gem.controller;
+
+import com.srilankagem.gembackend.gem.dto.TagRequest;
+import com.srilankagem.gembackend.gem.dto.TagResponse;
+import com.srilankagem.gembackend.gem.service.TagService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/tags")
+@RequiredArgsConstructor
+public class TagController {
+
+    private final TagService tagService;
+
+    @PostMapping
+    public ResponseEntity<TagResponse> createTag(
+            @Valid @RequestBody TagRequest request
+    ) {
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(tagService.createTag(request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TagResponse> getTagById(
+            @PathVariable Long id
+    ) {
+
+        return ResponseEntity.ok(
+                tagService.getTagById(id)
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<TagResponse>> getAllTags(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+
+        return ResponseEntity.ok(
+                tagService.getAllTags(page, size)
+        );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TagResponse> updateTag(
+            @PathVariable Long id,
+            @Valid @RequestBody TagRequest request
+    ) {
+
+        return ResponseEntity.ok(
+                tagService.updateTag(id, request)
+        );
+    }
+}
