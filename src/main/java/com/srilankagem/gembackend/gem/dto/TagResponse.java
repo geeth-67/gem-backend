d6690eb3e4bc6f3a5c8dc2e881813d637821ac44
vishwageethanjana <geeth.me67@gmail.com>
2026-06-01 11:43:10 +0530

    GemBackend java - M TO M relation 01.06.2026

---
+++ src/main/java/com/srilankagem/gembackend/gem/dto/TagResponse.java
@@ -0,0 +1,17 @@
+package com.srilankagem.gembackend.gem.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TagResponse {
+
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+}
