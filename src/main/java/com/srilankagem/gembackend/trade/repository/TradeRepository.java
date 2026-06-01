8dbf7e5a88dd13dd05a13936a89e3b790d39430b
vishwageethanjana <geeth.me67@gmail.com>
2026-06-01 10:46:22 +0530

    Gem backend java 01.06.2026

--- src/main/java/com/srilankagem/gembackend/trade/repository/TradeRepository.java
+++ src/main/java/com/srilankagem/gembackend/trade/repository/TradeRepository.java
@@ -1,4 +1,11 @@
 package com.srilankagem.gembackend.trade.repository;
 
-public interface TradeRepository {
+import com.srilankagem.gembackend.trade.entity.Trade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TradeRepository extends JpaRepository<Trade , Long> {
+
+
 }
